47e13a4366d5156bfb47608e8ce13675a04d7525
RajaKumar Goskula <108973030+raja9995@users.noreply.github.com>
2023-05-21 02:42:42 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,66 @@
+                                  //JAVA INTRODUCTION:-
+
+
+class Day1 //create className
+{
+  public static void main(String[] args) //Main method
+	{
+		System.out.println("Hello World!"); //print Statement
+
+        
+    //Create a variable and store the data Using DataTypes:
+		int a=100;
+		double d=21.3;
+		char ch='R';
+		String s="rajaa";
+		boolean b=true;
+			System.out.println(a);
+		    System.out.println(d);
+            System.out.println(ch);
+			System.out.println(s);
+			System.out.println(b);
+
+    //Variable Decleration and Variable Creation:
+	    int a1;
+		double d1;
+		char ch1;
+		String s1;
+		boolean b1;
+		a1=25;
+		d1=10.0;
+		ch1='g';
+		s1="hero";
+		b1=true;
+		    System.out.println(a1);
+		    System.out.println(d1);
+            System.out.println(ch1);
+			System.out.println(s1);
+			System.out.println(b1);
+
+	//Assigning Variable data to Another Variable:
+
+	    //ex1:
+		   int a2=50;
+		   int c;
+		   c=a2;
+		   System.out.println(a2);
+		   System.out.println(c);
+
+        //ex2:
+         String e1="hero";
+		 String e2="heroine";
+		 String e3="villain";
+		 String e4="don";
+		 e1=e2;
+		 e2=e3;
+		 e3=e1;
+		 e1=e3;
+		 e2=e1;
+		 e1=e3;
+		 System.out.println(e1);
+		 System.out.println(e2);
+         System.out.println(e3);
+         System.out.println(e4);
+
+	}
+}
